be9a9dd1fff5957152bf9d345afe907454fa237c
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-01 13:50:01 -0300

    ruta get albums

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+
+type album  struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Artist string `json:"artist"`
+	Price float64 `json:"price"`
+}
+
+//slice of albums
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+//controller
+func getAlbums(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
 
 func main(){
@@ -13,6 +35,9 @@ func main(){
 			"message": "Hello World!",
 		})
 	})
+
+	//GET ALBUMS
+	router.GET("/albums", getAlbums)
 
 	router.Run("localhost:8080")
 
